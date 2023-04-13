33f81ff2fc14190c198881e8e9db328f3e865d59
garry <Garry@>
2023-04-13 20:25:40 +0300

    init

---
+++ src/main/java/com/skypro/pastebin/PastebinApp.java
@@ -0,0 +1,13 @@
+package com.skypro.pastebin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastebinApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PastebinApp.class, args);
+    }
+
+}
